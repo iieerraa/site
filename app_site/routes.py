44b6b5e6893a59cb5afc5fe09cebe67dd15644c1
iieerraa <iieerraa2012@gmail.com>
2021-07-16 20:42:50 +0200

    add html

--- app_site/routes.py
+++ app_site/routes.py
@@ -1,7 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from flask import render_template
 from app_site import app
 
 
 @app.route('/')
-# @app.route('/index')
+@app.route('/index')
 def index():
-    return 'Hi!'
+    return render_template('index.html', title='Index')
+
+
+@app.route('/new/')
+@app.route('/new/<int:post_id>')
+def new(post_id=None):
+    return render_template('new.html', title=post_id)
--- web.py
+++ web.py
@@ -1,2 +1,5 @@
 from app_site import app
 
+# export/set FLASK_APP=site:name
+# export/set FLASK_ENV=development
+# flask run
