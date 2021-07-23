b5c8d9b31a6cbf567fc7f29c1fe1057bf095d48f
iieerraa <iieerraa2012@gmail.com>
2021-07-23 19:10:27 +0200

    getting started with the database

--- app_site/routes.py
+++ app_site/routes.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import render_template
+from flask import render_template, request
 from app_site import app
 
 
@@ -10,7 +10,18 @@ def index():
     return render_template('index.html', title='Index')
 
 
-@app.route('/new/')
-@app.route('/new/<int:post_id>')
+@app.route('/sign_up', methods=['post', 'get'])
+def sign_up():
+    message = ''
+    if request.method == 'POST':
+        username = request.form.get('email')
+        password = request.form.get('psw')
+        repeat_pas = request.form.get('psw_repeat')
+        message = 'Ok'
+
+    return render_template('sign_up.html', title='Sign Up', message=message)
+
+
+@app.route('/id/<int:post_id>')
 def new(post_id=None):
     return render_template('new.html', title=post_id)
--- web.py
+++ web.py
@@ -1,5 +1,6 @@
 from app_site import app
 
 # export/set FLASK_APP=site:name
-# export/set FLASK_ENV=development
+# export FLASK_APP=web.py
+# export FLASK_ENV=development
 # flask run
