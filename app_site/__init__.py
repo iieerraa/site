9d7931d51673ceb87839dd86fc13d69db8db6746
iieerraa <iieerraa2012@gmail.com>
2021-07-24 18:09:16 +0200

    getting started with the database.

--- app_site/__init__.py
+++ app_site/__init__.py
@@ -1,32 +1,12 @@
-import os.path
-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
-# app = Flask(__name__)
-# app.debug = True
-# app.config['SECRET_KEY'] = 'long_SECRET_KEY'
+app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'long_SECRET_KEY'
 
 
-def create_app(test_config=None):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='long_SECRET_KEY',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
-
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    from . import db
-    db.init_app(app)
-    return app
+db = SQLAlchemy(app)
 
 
 from app_site import routes
